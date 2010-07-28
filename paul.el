093ca7d28bfe257115e861e4f80d928695f111bb
Paul Stadig <paul@stadig.name>
2010-07-28 11:20:30 -0400

    Added customizations to paul.el; probably needs to be cleaned up, but for now ...

--- paul.el
+++ paul.el
@@ -137,3 +137,19 @@
          "* %?\n  Entered on %T\n  %i")))
 
 (setq org-agenda-files '("~/org"))
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(color-theme-is-cumulative t)
+ '(fill-column 80)
+ '(frame-background-mode (quote dark))
+ '(whitespace-line-column 80 t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "Inconsolata")))))
