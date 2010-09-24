edba371905e664e75fb5b44a015d86abfb8b2537
Paul Stadig <paul@stadig.name>
2010-09-24 15:13:01 -0400

    Updates for starting server and emasclient usage

--- common/50server.el
+++ common/50server.el
@@ -1,2 +1,16 @@
-(unless (boundp 'start-server)
-  (setq start-server t))
+(require 'server)
+(if (not (server-running-p))
+    (server-start))
+
+(defun exit-emacs-client ()
+  "consistent exit emacsclient.
+   if not in emacs client, echo a message in minibuffer, don't exit emacs.
+   if in server mode
+      and editing file, do C-x # server-edit
+      else do C-x 5 0 delete-frame"
+  (interactive)
+  (if server-buffer-clients
+      (server-edit)
+    (delete-frame)))
+
+(global-set-key (kbd "C-c q") 'exit-emacs-client)
