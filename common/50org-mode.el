9b3aa446dd4f1064f85cfd0cbc25cc6206a92227
Paul Stadig <paul@stadig.name>
2010-12-15 17:23:35 -0500

    Updates to orgmode configuration.

--- common/50org-mode.el
+++ common/50org-mode.el
@@ -5,10 +5,16 @@
 (define-key global-map "\C-cc" 'org-capture)
 (setq org-directory (concat home-dir "/org"))
 (setq org-agenda-files (list org-directory))
+(setq org-completion-use-ido 't)
+(setq org-refile-targets `((nil . (:level . 1))
+                           ((,(concat home-dir "/org/inbox.org")) .
+                            (:level . 1))
+                           ((,(concat home-dir "/org/projects.org")) .
+                            (:level . 1))))
 (setq org-capture-templates
       '(("t" "Todo" entry (file+headline "gtd.org" "Inbox")
          "* TODO %i%?")
-        ("p" "project" entry (file "gtd.org")
-         "* %i%? :project:")
+        ("p" "project" entry (file "projects.org")
+         "* %i%?")
         ("j" "Journal" entry (file+datetree "journal.org")
-         "* %i%?\n  Entered on %T")))
+         "* %i%?\n\n  Entered on %T")))
