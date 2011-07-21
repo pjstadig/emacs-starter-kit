8fcc7526f712959865f703167c7f7b784d93ad2f
Paul Stadig <paul@stadig.name>
2011-07-21 14:51:34 +0000

    Configure magit to switch to status buffer.

--- common/50magit.el
+++ common/50magit.el
@@ -1,3 +1,4 @@
 (when-load-path (concat home-dir "/src/magit")
   (require 'magit)
-  (setq magit-diff-options '("-b")))
+  (setq magit-diff-options '("-b"))
+  (setq magit-status-buffer-switch-function 'switch-to-buffer))
