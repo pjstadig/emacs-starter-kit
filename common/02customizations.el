d849e416e598fe92a6a912570b39e01b64f67fba
Paul Stadig <paul@stadig.name>
2011-12-28 17:44:54 -0500

    Bump the font down to 11.

--- common/02customizations.el
+++ common/02customizations.el
@@ -4,11 +4,11 @@
       whitespace-line-column 80)
 
 (modify-frame-parameters nil '((wait-for-wm . nil)))
-(set-face-attribute 'default nil :family "Inconsolata" :height 120)
+(set-face-attribute 'default nil :family "Inconsolata" :height 110)
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "Inconsolata")))))
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "unknown" :family "Inconsolata")))))
